11218f2f6a618f2fc434d54a30b08afd01ddfbd5
Computer Acer <mariustucureanua@gmail.com>
2023-05-29 13:33:44 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab libero
+        perspiciatis error enim quis, tenetur doloremque dignissimos? Mollitia
+        officia accusamus similique ipsa animi pariatur odit cupiditate,
+        deserunt commodi minima dolorem iure at laudantium nisi dolores! Veniam
+        optio odit vel cum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
